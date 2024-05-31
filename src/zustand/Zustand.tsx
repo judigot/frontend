a698bfd1cd097ec0c8468e912e2dd54484a8b935
judigot <judigot@gmail.com>
2024-05-31 22:45:18 +0800

    Update

--- src/zustand/Zustand.tsx
+++ src/zustand/Zustand.tsx
@@ -1,13 +1,21 @@
-import { useCounterStore } from '@/zustand/store';
+import { useStore } from '@/zustand/store';
 
-export default function Zustand() {
-  const { count, increment } = useCounterStore();
+export default function App() {
+  const { count, increment, searchQuery, setSearchQuery } = useStore();
 
   return (
     <>
       <h1>Zustand</h1>
       <button type="button" onMouseDown={increment}>
         Count: {count}
+      </button>
+      <button
+        type="button"
+        onMouseDown={() => {
+          setSearchQuery('newValue');
+        }}
+      >
+        Search Query: {searchQuery}
       </button>
     </>
   );
--- src/zustand/store.ts
+++ src/zustand/store.ts
@@ -1,13 +1,19 @@
 import { create } from 'zustand';
 
-interface ICounterStore {
+interface Store {
   count: number;
   increment: () => void;
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
 }
 
-export const useCounterStore = create<ICounterStore>()((set) => ({
-  count: 0,
+export const useStore = create<Store>()((set) => ({
+  count: 1,
   increment: () => {
     set(({ count }) => ({ count: count + 1 }));
+  },
+  searchQuery: 'Initial value',
+  setSearchQuery: (query: string) => {
+    set({ searchQuery: query });
   },
 }));
